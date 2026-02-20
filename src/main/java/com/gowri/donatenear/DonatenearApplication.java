4741de6118ae48b5047a2b6c1844bc18f0827cd2
Gowri K <gowrigk@amazon.com>
2026-02-20 16:01:15 +0530

    Initial Spring Boot 3.5.12 setup with Spring Web

---
+++ src/main/java/com/gowri/donatenear/DonatenearApplication.java
@@ -0,0 +1,13 @@
+package com.gowri.donatenear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatenearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatenearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowri/donatenear/DonatenearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri.donatenear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatenearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
